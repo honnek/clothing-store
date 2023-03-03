3d4b83912248d5b68988af7995d0da7f649524d3
honnek <leonardo_007@inbox.ru>
2023-03-03 20:38:36 +0300

    Добавлен webpack

---
+++ assets/section-admin.js
@@ -0,0 +1,14 @@
+const $ = require("jquery")
+require("bootstrap")
+
+global.$ = global.jquery = $
+
+require("jquery.easing")
+require("chart.js")
+
+require('./js/section/admin/theme/sb-admin-2')
+
+
+import './css/section/admin/libs.scss'
+import './css/section/admin/sb-admin-2.css'
+import './css/section/admin/styles.css'
