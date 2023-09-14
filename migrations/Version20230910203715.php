87eed649de0ac78d9c9465b9835a5e6a37ad3df0
honnek <leonardo_007@inbox.ru>
2023-09-14 13:49:14 +0300

    исправлена миграция

--- migrations/Version20230910203715.php
+++ migrations/Version20230910203715.php
@@ -9,6 +9,7 @@ use Doctrine\Migrations\AbstractMigration;
 
 /**
  * Auto-generated Migration: Please modify to your needs!
+ * @TODO несколько запросов убрано для накатывания теста
  */
 final class Version20230910203715 extends AbstractMigration
 {
@@ -20,11 +21,11 @@ final class Version20230910203715 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP SEQUENCE test_table_id_seq CASCADE');
+//        $this->addSql('DROP SEQUENCE test_table_id_seq CASCADE');
         $this->addSql('DROP SEQUENCE messenger_messages_id_seq CASCADE');
         $this->addSql('CREATE SEQUENCE test_api_entity_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE TABLE test_api_entity (id INT NOT NULL, title VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, is_deleted BOOLEAN NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('DROP TABLE test_table');
+//        $this->addSql('DROP TABLE test_table');
         $this->addSql('DROP TABLE messenger_messages');
         $this->addSql('ALTER TABLE "order" ALTER owner_id DROP NOT NULL');
     }
@@ -34,9 +35,9 @@ final class Version20230910203715 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
         $this->addSql('DROP SEQUENCE test_api_entity_id_seq CASCADE');
-        $this->addSql('CREATE SEQUENCE test_table_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+//        $this->addSql('CREATE SEQUENCE test_table_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE SEQUENCE messenger_messages_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE TABLE test_table (id SERIAL NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+//        $this->addSql('CREATE TABLE test_table (id SERIAL NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE TABLE messenger_messages (id BIGSERIAL NOT NULL, body TEXT NOT NULL, headers TEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, available_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, delivered_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX idx_75ea56e016ba31db ON messenger_messages (delivered_at)');
         $this->addSql('CREATE INDEX idx_75ea56e0e3bd61ce ON messenger_messages (available_at)');
