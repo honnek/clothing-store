c807a92dc69dab2d26dc296bfaa447052147ee1c
honnek <leonardo_007@inbox.ru>
2023-09-15 13:41:23 +0300

    DefaultControllerTest тесты

---
+++ tests/Functional/Controller/Main/DefaultControllerTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Tests\Functional\Controller\Main;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @group functional
+ */
+class DefaultControllerTest extends WebTestCase
+{
+    public function testRedirectEmptyUrlToLocale(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertResponseRedirects(
+            'http://localhost/en',
+            Response::HTTP_MOVED_PERMANENTLY,
+            sprintf('The %s URL redirects to the version with locale', '/')
+        );
+    }
+
+    /**
+     * @dataProvider getPublicUrls
+     * @param string $url
+     * @return void
+     */
+    public function testPublicUrls(string $url)
+    {
+        $client = static::createClient();
+
+        $client->request('GET', $url);
+
+        $this->assertResponseIsSuccessful(sprintf('Public URL %s loads correctly', $url));
+    }
+
+    /**
+     * @dataProvider getSecurityUrls
+     * @param string $url
+     * @return void
+     */
+    public function testSecurityUrls(string $url)
+    {
+        $client = static::createClient();
+
+        $client->request('GET', $url);
+
+        $this->assertResponseRedirects(
+            '/en/login',
+            Response::HTTP_FOUND,
+            sprintf('The %s URL redirects to the login page', '/')
+        );    }
+
+    public function getPublicUrls(): ?\Generator
+    {
+        yield ['/en/'];
+        yield ['/en/login'];
+        yield ['/en/registration'];
+        yield ['/en/reset-password'];
+    }
+
+    public function getSecurityUrls(): ?\Generator
+    {
+        yield ['/en/profile'];
+        yield ['/en/profile/edit'];
+    }
+}
